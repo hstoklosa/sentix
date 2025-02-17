55477de5e2dc814312e6ae106c70bb10d4bf536c
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-17 02:19:20 +0000

    wip: construct initial setup for storing backend settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,62 @@
+from typing import Annotated, Any, Literal
+
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    computed_field,
+    PostgresDsn
+)
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+# REF: https://github.com/fastapi/full-stack-fastapi-template
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
+
+class Settings(BaseSettings):    
+    PROJECT_NAME: str = "SentiX"
+    API_VERSION: str = "/api/v1"
+    FRONTEND_HOST: str = "http://localhost:5173"
+    
+    app_name: str = "SentiX: Backend"
+    admin_email: str = "hubert.stoklosa23@gmail.com"
+
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+
+    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = []
+
+    @computed_field
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
+    
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+    
+settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,16 @@
 from typing import Union
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    openapi_url=f"{settings.API_VERSION}/openapi.json",
+)
 
 @app.get("/")
 def read_root():
-    return { "Hello": "World" }
+    return { "API_NAME": settings.API_VERSION }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
