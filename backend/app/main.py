069402496bbec83b0de5aa4b8556dbcfe14e6cef
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-19 04:31:10 +0000

    remove unnecessary code from backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,11 +7,3 @@ app = FastAPI(
     title=settings.app_name,
     openapi_url=f"{settings.API_VERSION}/openapi.json",
 )
-
-@app.get("/")
-def read_root():
-    return { "API_NAME": settings.API_VERSION }
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return { "item_id": item_id, "q": q }
