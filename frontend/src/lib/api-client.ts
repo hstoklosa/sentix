fa51f397e59315dcb2224380b581059e3731f341
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-19 15:45:56 +0000

    set JSON as the default content type within lib/api-client

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,5 +1,19 @@
-import axios, { AxiosInstance } from "axios";
+import axios, {
+  AxiosInstance,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from "axios";
 
 export const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    config.headers["Content-Type"] = "application/json";
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
