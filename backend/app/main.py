8b754233d5af113c346864d758a035c4443ac6bd
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-19 06:45:37 +0000

    initialise a database connection on backend startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
-from typing import Union
 from fastapi import FastAPI
 
+from app.core.db import create_db_and_tables
 from app.core.config import settings
 
 app = FastAPI(
     title=settings.app_name,
     openapi_url=f"{settings.API_VERSION}/openapi.json",
 )
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
