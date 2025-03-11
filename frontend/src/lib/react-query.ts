543385377ba33cce69231ddc75e8a88bcbceb21a
imexotic <hubert.stoklosa23@gmail.com>
2025-03-11 03:29:56 +0000

    chore(libs): export types from lib/react-query for queries and mutations

--- frontend/src/lib/react-query.ts
+++ frontend/src/lib/react-query.ts
@@ -1,4 +1,9 @@
-import { DefaultOptions, QueryClient } from "@tanstack/react-query";
+import {
+  DefaultOptions,
+  QueryClient,
+  UseMutationOptions,
+} from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 export const queryConfig = {
   queries: {
@@ -12,3 +17,19 @@ export const createQueryClient = () =>
   new QueryClient({
     defaultOptions: queryConfig,
   });
+
+// REF: https://github.com/alan2207/bulletproof-react/blob/master/apps/react-vite/src/lib/react-query.ts
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> =
+  Awaited<ReturnType<FnType>>;
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+  ReturnType<T>,
+  "queryKey" | "queryFn"
+>;
+
+export type MutationConfig<MutationFnType extends (...args: any) => Promise<any>> =
+  UseMutationOptions<
+    ApiFnReturnType<MutationFnType>,
+    AxiosError,
+    Parameters<MutationFnType>[0]
+  >;
