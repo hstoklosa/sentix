11b9ff2214014c91bc1c7add26c702545a912031
imexotic <hubert.stoklosa23@gmail.com>
2025-03-13 13:13:19 +0000

    refactor(news): improve code organisation for receiving live data from provider

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,9 +1,18 @@
 from typing import Any
+from datetime import datetime, timezone
 
-# REF: https://github.com/fastapi/full-stack-fastapi-template
 def parse_cors(v: Any) -> list[str] | str:
+    """
+    Parse CORS header value to string or list of strings
+    """
     if isinstance(v, str) and not v.startswith("["):
         return [i.strip() for i in v.split(",")]
     elif isinstance(v, list | str):
         return v
     raise ValueError(v)
+
+def datetime_from_timestamp(timestamp: int) -> datetime:
+    """
+    Convert timestamp from milliseconds to datetime object
+    """
+    return datetime.fromtimestamp(timestamp / 1000, tz=timezone.utc)
