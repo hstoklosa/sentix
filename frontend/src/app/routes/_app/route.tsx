76549b80960f1bf161d2a0b84c57e34bd789afa4
imexotic <hubert.stoklosa23@gmail.com>
2025-03-31 23:54:21 +0100

    fix(ui): resolve issues like wrong redirections based on auth state

--- frontend/src/app/routes/_app/route.tsx
+++ frontend/src/app/routes/_app/route.tsx
@@ -1,18 +1,6 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
-import { Spinner } from "@/components/ui";
 
 const DashboardLayout = () => {
-  const { auth } = Route.useRouteContext();
-
-  if (auth.status === "PENDING") {
-    return (
-      <Spinner
-        size="lg"
-        fullScreen
-      />
-    );
-  }
-
   return (
     <div>
       <Outlet />
@@ -22,7 +10,6 @@ const DashboardLayout = () => {
 
 export const Route = createFileRoute("/_app")({
   beforeLoad: ({ context }) => {
-    // Only redirect if explicitly UNAUTHENTICATED (proceed to component if PENDING)
     if (context.auth.status === "UNAUTHENTICATED") {
       throw redirect({
         to: "/login",
