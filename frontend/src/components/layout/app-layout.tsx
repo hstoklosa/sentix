12f41f61fa772ecdc96ebb9f7a2d79239b6bf2ff
imexotic <hubert.stoklosa23@gmail.com>
2025-04-02 09:00:37 +0100

    chore(ui): create header within the dashboard layout

--- frontend/src/components/layout/app-layout.tsx
+++ frontend/src/components/layout/app-layout.tsx
@@ -1,8 +1,61 @@
+import sentixLogo from "@/assets/sentix-logo.png";
+import useAuth from "@/hooks/use-auth";
+
+import { Button } from "../ui";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "../ui/dropdown-menu";
+
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useAuth();
+
   return (
-    <div className="container mx-auto">
-      <header>
-        <h1>Dashboard</h1>
+    <div className="">
+      <header className="flex justify-between items-center h-[56px] px-1">
+        <div className="flex items-center">
+          <img
+            src={sentixLogo}
+            alt="Sentix Logo"
+            className="h-8"
+          />
+          <h1 className="text-sm font-[Inter] ml-1">SENTIX</h1>
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center">
+              <Button
+                variant="ghost"
+                size="sm"
+              >
+                {user?.username}
+              </Button>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            side="bottom"
+            sideOffset={12}
+          >
+            <DropdownMenuLabel>
+              <p className="text-sm font-normal text-foreground">
+                {user?.username}
+              </p>
+              <p className="text-sm font-normal text-muted-foreground">
+                {user?.email}
+              </p>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </header>
 
       <main>{children}</main>
