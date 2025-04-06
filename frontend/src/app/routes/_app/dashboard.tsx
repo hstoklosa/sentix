2badbff040b4b6549b606342a0d8d79aaf0dbf6f
imexotic <hubert.stoklosa23@gmail.com>
2025-04-06 01:54:31 +0100

    chore(ui): modify grid layout on the dashboard

--- frontend/src/app/routes/_app/dashboard.tsx
+++ frontend/src/app/routes/_app/dashboard.tsx
@@ -1,24 +1,29 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import Logo from "@/components/logo";
 import { NewsList } from "@/features/news/components";
 
 function RouteComponent() {
   return (
-    <div className="grid grid-cols-[repeat(3,1fr)] grid-rows-[repeat(3,1fr)] gap-x-2 gap-y-2 h-[calc(100vh-4.5rem)] overflow-hidden">
-      <div className="row-start-1 row-end-4 col-start-1 col-end-2 flex flex-col border-1 border-border rounded-md bg-card">
+    <div className="grid grid-cols-[repeat(3,1fr)] grid-rows-[repeat(4,1fr)] gap-x-2 gap-y-2 h-[calc(100vh-4.5rem)] overflow-hidden">
+      <div className="row-start-1 row-end-5 col-start-1 col-end-2 flex flex-col border-1 border-border rounded-md bg-card">
         <NewsList />
       </div>
 
-      <div className="row-start-1 row-end-2 col-start-2 col-end-4 flex flex-col border-1 border-border rounded-md bg-card">
-        2
+      <div className="row-start-1 row-end-3 col-start-2 col-end-3 h-full flex flex-col justify-center items-center border-1 border-border rounded-md bg-card">
+        <Logo className="size-16" />
       </div>
 
-      <div className="row-start-2 row-end-4 col-start-2 col-end-3 flex flex-col border-1 border-border rounded-md bg-card">
-        3
+      <div className="row-start-3 row-end-5 col-start-2 col-end-3 h-full flex flex-col justify-center items-center border-1 border-border rounded-md bg-card">
+        <Logo className="size-16" />
       </div>
 
-      <div className="row-start-2 row-end-4 col-start-3 col-end-4 flex flex-col border-1 border-border rounded-md bg-card">
-        4
+      <div className="row-start-1 row-end-2 col-start-3 col-end-4 h-full flex flex-col justify-center items-center border-1 border-border rounded-md bg-card">
+        <Logo className="size-16" />
+      </div>
+
+      <div className="row-start-2 row-end-5 col-start-3 col-end-4 h-full flex flex-col justify-center items-center border-1 border-border rounded-md bg-card">
+        <Logo className="size-16" />
       </div>
     </div>
   );
