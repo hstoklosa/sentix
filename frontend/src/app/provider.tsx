9ebb3909358b11a01cf864b5e1f8c1a90da38da1
imexotic <hubert.stoklosa23@gmail.com>
2025-04-13 04:41:02 +0100

    chore(deps): add Toaster from sonner library into the project root

--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -1,6 +1,9 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { createQueryClient } from "@/lib/react-query";
+
+import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/hooks/use-theme";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
@@ -8,6 +11,7 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <ThemeProvider>
+      <Toaster richColors={true} />
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools
           initialIsOpen={false}
