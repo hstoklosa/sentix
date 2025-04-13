c545166ee557b4393621805548f6f2faeeba7cbd
imexotic <hubert.stoklosa23@gmail.com>
2025-04-13 03:31:55 +0100

    fix(ui): add inner scroll to the contents of post route excluding the header

--- frontend/src/app/routes/_app/dashboard.$postId.tsx
+++ frontend/src/app/routes/_app/dashboard.$postId.tsx
@@ -108,110 +108,106 @@ function PostComponent() {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col gap-2">
-        <div className="flex items-center gap-2 px-2.5 py-2 border-b border-border">
-          <div
-            className={cn(
-              "flex justify-center items-center min-w-8 min-h-8 rounded-full bg-primary/10",
-              post.source === "Twitter" ? "bg-black" : "bg-[#7233F7]"
-            )}
-          >
-            <img
-              src={post.source === "Twitter" ? xIcon : newsIcon}
-              alt="X"
-              className="size-4"
-            />
-          </div>
-          <div className="flex flex-col text-xs">
-            <div className="flex items-center gap-1">
-              <span className="text-sm text-foreground capitalize">
-                {post.source}
-              </span>
-              <a
-                aria-label="Read more"
-                href={post.url}
-                target="_blank"
-                className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors"
-                rel="noopener noreferrer"
-              >
-                <ExternalLink className="size-3.5" />
-              </a>
-            </div>
-            <span className="text-muted-foreground">
-              {formatDateTime(post.time)}
+    <div className="flex flex-col h-full">
+      <div className="flex items-center gap-2 px-2.5 py-2 border-b border-border">
+        <div
+          className={cn(
+            "flex justify-center items-center min-w-8 min-h-8 rounded-full bg-primary/10",
+            post.source === "Twitter" ? "bg-black" : "bg-[#7233F7]"
+          )}
+        >
+          <img
+            src={post.source === "Twitter" ? xIcon : newsIcon}
+            alt="X"
+            className="size-4"
+          />
+        </div>
+        <div className="flex flex-col text-xs">
+          <div className="flex items-center gap-1">
+            <span className="text-sm text-foreground capitalize">
+              {post.source}
             </span>
-          </div>
-
-          <div className="ml-auto flex items-center gap-3">
-            <button
-              aria-label={post.is_bookmarked ? "Remove bookmark" : "Add bookmark"}
-              title={post.is_bookmarked ? "Remove bookmark" : "Add bookmark"}
-              className={cn(
-                "flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors",
-                post.is_bookmarked && "text-primary"
-              )}
-              onClick={handleBookmarkToggle}
-            >
-              <Bookmark
-                className="size-3.5"
-                fill={post.is_bookmarked ? "currentColor" : "none"}
-              />
-            </button>
-            <button
-              aria-label="Copy Title"
+            <a
+              aria-label="Read more"
+              href={post.url}
+              target="_blank"
               className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors"
-              onClick={(_) => handleCopy(post.title)}
+              rel="noopener noreferrer"
             >
-              <Copy className="size-3.5" />
-            </button>
-            <button
-              aria-label="Copy URL"
-              className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors"
-              onClick={(_) => handleCopy(post.url)}
-            >
-              <LinkIcon className="size-3.5" />
-            </button>
-
-            {/* <div className="h-[18px] w-[1px] rounded-full bg-muted-foreground/50 ml-1.5" /> */}
-
-            <Link
-              to="/dashboard"
-              className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors ml-[-4px]"
-            >
-              <X className="size-4.5" />
-            </Link>
+              <ExternalLink className="size-3.5" />
+            </a>
           </div>
+          <span className="text-muted-foreground">{formatDateTime(post.time)}</span>
+        </div>
+
+        <div className="ml-auto flex items-center gap-3">
+          <button
+            aria-label={post.is_bookmarked ? "Remove bookmark" : "Add bookmark"}
+            title={post.is_bookmarked ? "Remove bookmark" : "Add bookmark"}
+            className={cn(
+              "flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors",
+              post.is_bookmarked && "text-primary"
+            )}
+            onClick={handleBookmarkToggle}
+          >
+            <Bookmark
+              className="size-3.5"
+              fill={post.is_bookmarked ? "currentColor" : "none"}
+            />
+          </button>
+          <button
+            aria-label="Copy Title"
+            className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors"
+            onClick={(_) => handleCopy(post.title)}
+          >
+            <Copy className="size-3.5" />
+          </button>
+          <button
+            aria-label="Copy URL"
+            className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors"
+            onClick={(_) => handleCopy(post.url)}
+          >
+            <LinkIcon className="size-3.5" />
+          </button>
+
+          <Link
+            to="/dashboard"
+            className="flex items-center justify-center rounded-sm text-muted-foreground hover:text-primary transition-colors ml-[-4px]"
+          >
+            <X className="size-4.5" />
+          </Link>
         </div>
       </div>
 
-      <div className="flex flex-col gap-2 p-2.5 ">
-        <h2 className="text-xl font-bold">{post.title}</h2>
-        {post.image_url && (
-          <img
-            src={post.image_url}
-            alt={post.title}
-            className="rounded-lg w-full max-h-80 object-cover"
-          />
-        )}
+      <div className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-muted-foreground/20 scrollbar-track-transparent">
+        <div className="flex flex-col gap-2 p-2.5">
+          <h2 className="text-xl font-bold">{post.title}</h2>
+          {post.image_url && (
+            <img
+              src={post.image_url}
+              alt={post.title}
+              className="rounded-lg w-full max-h-80 object-cover"
+            />
+          )}
 
-        <div className="prose prose-sm dark:prose-invert line-break-anywhere">
-          {post.body ? post.body : post.title}
-        </div>
-
-        {post.coins.length > 0 && (
-          <div className="mt-2">
-            <h3 className="text-sm font-medium mb-2">Related Coins:</h3>
-            <div className="flex flex-wrap gap-2">
-              {post.coins.map((coin) => (
-                <CoinTag
-                  key={coin.id}
-                  symbol={coin.symbol}
-                />
-              ))}
-            </div>
+          <div className="prose prose-sm dark:prose-invert line-break-anywhere">
+            {post.body ? post.body : post.title}
           </div>
-        )}
+
+          {post.coins.length > 0 && (
+            <div className="mt-2">
+              <h3 className="text-sm font-medium mb-2">Related Coins:</h3>
+              <div className="flex flex-wrap gap-2">
+                {post.coins.map((coin) => (
+                  <CoinTag
+                    key={coin.id}
+                    symbol={coin.symbol}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
